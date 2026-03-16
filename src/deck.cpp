3afce67bf70534c068e9a6269fba93c8d3f63f71
PlasmaChroma <plasmachroma@gmail.com>
2026-03-16 14:25:48 -0400

    Fix adding deck to plugin source

--- src/deck.cpp
+++ src/deck.cpp
@@ -60,7 +60,7 @@ struct TemporalDeck : Module {
 struct TemporalDeckWidget : ModuleWidget {
 	TemporalDeckWidget(TemporalDeck* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/TemporalDeck.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/deck.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
