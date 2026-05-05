04ea3448ee90ff4a6f05d86e3a8abc3284dad602
Levi Kendall <PlasmaChroma@gmail.com>
2026-05-04 21:04:24 -0400

    Add first pass of waveform display

--- src/Sil.cpp
+++ src/Sil.cpp
@@ -1,5 +1,7 @@
 #include "plugin.hpp"
 #include "PanelSvgUtils.hpp"
+#include <vector>
+#include <algorithm>
 
 struct Sil : Module {
 	enum ParamId {
@@ -19,22 +21,116 @@ struct Sil : Module {
 		LIGHTS_LEN
 	};
 
+	static constexpr int HISTOGRAM_BINS = 1000;
+	static constexpr float HISTOGRAM_DURATION = 10.f;
+
+	struct HistogramData {
+		float minL[HISTOGRAM_BINS] = {};
+		float maxL[HISTOGRAM_BINS] = {};
+		float minR[HISTOGRAM_BINS] = {};
+		float maxR[HISTOGRAM_BINS] = {};
+		int writePtr = 0;
+
+		float currentMinL = 1e10f, currentMaxL = -1e10f;
+		float currentMinR = 1e10f, currentMaxR = -1e10f;
+		int samplesInCurrentBin = 0;
+		int samplesPerBin = 441;
+	} hist;
+
 	Sil() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
 		configInput(INPUT_L_INPUT, "Left");
 		configInput(INPUT_R_INPUT, "Right");
 		configOutput(OUTPUT_L_OUTPUT, "Left");
 		configOutput(OUTPUT_R_OUTPUT, "Right");
+
+		hist.samplesPerBin = (int)(APP->engine->getSampleRate() * HISTOGRAM_DURATION / HISTOGRAM_BINS);
+	}
+
+	void onSampleRateChange(const SampleRateChangeEvent& e) override {
+		hist.samplesPerBin = (int)(e.sampleRate * HISTOGRAM_DURATION / HISTOGRAM_BINS);
+		if (hist.samplesPerBin < 1) hist.samplesPerBin = 1;
 	}
 
 	void process(const ProcessArgs& args) override {
-		(void) args;
 		const float inL = inputs[INPUT_L_INPUT].getVoltage();
 		const float inR = inputs[INPUT_R_INPUT].getVoltage();
 		outputs[OUTPUT_L_OUTPUT].setChannels(1);
 		outputs[OUTPUT_R_OUTPUT].setChannels(1);
 		outputs[OUTPUT_L_OUTPUT].setVoltage(inL);
 		outputs[OUTPUT_R_OUTPUT].setVoltage(inR);
+
+		// Update histogram
+		hist.currentMinL = std::min(hist.currentMinL, inL);
+		hist.currentMaxL = std::max(hist.currentMaxL, inL);
+		hist.currentMinR = std::min(hist.currentMinR, inR);
+		hist.currentMaxR = std::max(hist.currentMaxR, inR);
+		hist.samplesInCurrentBin++;
+
+		if (hist.samplesInCurrentBin >= hist.samplesPerBin) {
+			hist.minL[hist.writePtr] = hist.currentMinL;
+			hist.maxL[hist.writePtr] = hist.currentMaxL;
+			hist.minR[hist.writePtr] = hist.currentMinR;
+			hist.maxR[hist.writePtr] = hist.currentMaxR;
+
+			hist.writePtr = (hist.writePtr + 1) % HISTOGRAM_BINS;
+
+			hist.currentMinL = 1e10f; hist.currentMaxL = -1e10f;
+			hist.currentMinR = 1e10f; hist.currentMaxR = -1e10f;
+			hist.samplesInCurrentBin = 0;
+		}
+	}
+};
+
+struct HistogramWidget : TransparentWidget {
+	Sil* module;
+
+	void draw(const DrawArgs& args) override {
+		if (!module) return;
+
+		// Draw background (black)
+		nvgBeginPath(args.vg);
+		nvgRect(args.vg, 0, 0, box.size.x, box.size.y);
+		nvgFillColor(args.vg, nvgRGBA(0, 0, 0, 255));
+		nvgFill(args.vg);
+
+		float midY = box.size.y / 2.f;
+		float halfH = box.size.y / 4.f;
+
+		// Cyan color matching the border
+		NVGcolor waveColor = nvgRGBA(0x1c, 0xca, 0xd8, 0xff);
+
+		auto drawChannel = [&](const float* minBuf, const float* maxBuf, float centerY) {
+			nvgBeginPath(args.vg);
+			for (int i = 0; i < Sil::HISTOGRAM_BINS; i++) {
+				int idx = (module->hist.writePtr + i) % Sil::HISTOGRAM_BINS;
+				float x = (float)i / (Sil::HISTOGRAM_BINS - 1) * box.size.x;
+
+				// Scale assuming +/- 5V range
+				float valMin = clamp(minBuf[idx] / 5.f, -1.f, 1.f);
+				float valMax = clamp(maxBuf[idx] / 5.f, -1.f, 1.f);
+
+				float yMin = centerY - valMin * halfH;
+				float yMax = centerY - valMax * halfH;
+
+				nvgMoveTo(args.vg, x, yMin);
+				nvgLineTo(args.vg, x, yMax);
+			}
+			nvgStrokeColor(args.vg, waveColor);
+			nvgStrokeWidth(args.vg, 1.0f);
+			nvgStroke(args.vg);
+		};
+
+		drawChannel(module->hist.minL, module->hist.maxL, midY * 0.5f);
+		drawChannel(module->hist.minR, module->hist.maxR, midY * 1.5f);
+
+		// Divider
+		nvgBeginPath(args.vg);
+		nvgMoveTo(args.vg, 0, midY);
+		nvgLineTo(args.vg, box.size.x, midY);
+		nvgStrokeColor(args.vg, nvgRGBA(0x1c, 0xca, 0xd8, 0x40));
+		nvgStrokeWidth(args.vg, 0.5f);
+		nvgStroke(args.vg);
 	}
 };
 
@@ -49,6 +145,16 @@ struct SilWidget : ModuleWidget {
 		setModule(module);
 		const std::string panelPath = asset::plugin(pluginInstance, "res/sil.svg");
 		setPanel(createPanel(asset::plugin(pluginInstance, "res/sil.svg")));
+
+		math::Rect histRect;
+		if (panel_svg::loadRectFromSvgMm(panelPath, "HISTOGRAM", &histRect)) {
+			// Inset slightly to stay inside the cyan border
+			histRect = histRect.grow(Vec(-0.2f, -0.2f));
+			HistogramWidget* hw = createWidget<HistogramWidget>(mm2px(histRect.pos));
+			hw->box.size = mm2px(histRect.size);
+			hw->module = module;
+			addChild(hw);
+		}
 
 		Vec inputLPos(26.f, 118.f);
 		Vec inputRPos(42.f, 118.f);
