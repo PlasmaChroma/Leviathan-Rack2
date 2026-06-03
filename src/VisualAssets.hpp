9535810de76d6c4aae6a68d7ce64eab0dc977119
PlasmaChroma <plasmachroma@gmail.com>
2026-06-03 16:08:10 -0400

    Adding semi-fucked up shadow

--- src/VisualAssets.hpp
+++ src/VisualAssets.hpp
@@ -10,10 +10,7 @@ struct GearKnobInvertSized : app::SvgKnob {
 		setSvg(Svg::load(asset::plugin(pluginInstance, "res/icon/gear_knob_invert.svg")));
 		box.size = Vec(46.f, 46.f);
 		if (shadow) {
-			shadow->opacity = 0.3f;
-			shadow->blurRadius = 10.f;
-			shadow->box.pos = Vec(3.f, 3.f);
-			shadow->box.size = Vec(40.f, 40.f);
+			shadow->opacity = 0.f;
 		}
 	}
 };
